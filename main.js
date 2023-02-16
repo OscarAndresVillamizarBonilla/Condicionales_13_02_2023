4e62cb4dfaac765eef38cf78bec186adfc496bce
OscarAndresVillamizarBonilla <andrex.villamizar1@gmail.com>
2023-02-16 17:19:25 -0500

    If simple

---
+++ main.js
@@ -0,0 +1,8 @@
+//if simple
+// el if simple es para acciones simples sin alternativa, solo hay una desicion
+let num = Number(prompt("Aca puedes obtener el resultado de un multiplo de 2, escribe un numero"));
+let resultado;
+if (num){
+    resultado = num*2;
+console.log(`Has ingresado el numero ${num} si lo multiplicamos por 2, sera ${resultado}`);
+};
