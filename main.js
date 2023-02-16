61616da4fabe1aee907d382d9473055ee72f0ee9
OscarAndresVillamizarBonilla <andrex.villamizar1@gmail.com>
2023-02-16 18:25:32 -0500

    If anidado

--- main.js
+++ main.js
@@ -4,17 +4,17 @@ let genero = Number(prompt(`Vamos a validar si tenemos tu talla de zapatos \n\t\
 if (genero == 1){
     let talla1 = Number(prompt(`Vamos a validar tu talla de zapatos de Mujer, escribe tu talla`));
     if (talla1>=32 && talla1<=38)
-    console.log(`Tu talla es ${talla1}, si tenemos tu talla disponible`);
+    console.log(`tu talla es ${talla1}, si tenemos tu talla disponible`);
 }
     else {
         if (genero == 2){
             let talla2 = Number(prompt(`Vamos a validar tu talla de zapatos de Hombre, escribe tu talla`));
 if (talla2>=39 && talla2<=43) {
-    console.log(`Tu talla es ${talla2}, si tenemos tu talla disponible`);
+    console.log(`tu talla es ${talla2}, si tenemos tu talla disponible`);
 }
 
 else {
-    console.log(`No tenemos tu talla disponible`);
+    console.log(`No tenemos tu talla disponiblela tenemos disponible`);
             }
          }
     }
